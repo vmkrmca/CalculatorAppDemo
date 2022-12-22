608cee4a86f9b153e9ed625ad8e9bb826a278732
ManiDevops <manireddydevops@gmail.com>
2022-12-22 13:15:04 +0530

    Second Commit

--- app/src/main/java/com/deepak/calculatorapplicationdemo/MainActivity.kt
+++ app/src/main/java/com/deepak/calculatorapplicationdemo/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Adding a Line of COde in Android studio
     }
 }
